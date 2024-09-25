2f8455ace4a8e5cc0dc372f61cb96ec4509f9845
GSheldon262 <gsheldon@ltu.edu>
2024-09-24 23:14:42 -0400

    Bullets and Enemy Destruction

--- Assets/Scripts/PlayerBulletScript.cs
+++ Assets/Scripts/PlayerBulletScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -13,6 +14,8 @@ public class PlayerBulletScript : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
     }
 
     // Update is called once per frame
@@ -20,7 +23,16 @@ public class PlayerBulletScript : MonoBehaviour
     {
         body.velocity = new Vector2(0,speed);
 
+        
+
     }
 
     //destroy bullet when touching endpoint
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "End")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/WaveObject.cs
+++ Assets/Scripts/WaveObject.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class WaveObject : MonoBehaviour
 {
     public int waveOf = 0;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
